8e7930f1ba99c3ecec34f1028784ee9c4113c022
sebslon <sebastesterprogram@gmail.com>
2021-01-28 19:41:54 +0100

    Add simple authentication

---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+const _ = require('lodash')
+const {User} = require('../models/user');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+router.get('/', async (req,res) => {
+  const users = await User.find().sort('name');
+  res.send(users)
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if(!user) return res.status(400).send('Invalid email or password...');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if(!validPassword) return res.status(400).send('Invalid email or password...');
+
+  //JWT
+  res.send(true);
+});
+
+function validate(req) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  });
+
+  return schema.validate(req);
+}
+
+module.exports = router;
