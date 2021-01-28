344b26656c725d51e51d30dbca378104e56ecb1b
sebslon <sebastesterprogram@gmail.com>
2021-01-28 19:14:10 +0100

    Use bcrypt library to hash user password

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,14 @@
+const _ = require('lodash')
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
+router.get('/', async (req,res) => {
+  const users = await User.find().sort('name');
+  res.send(users)
+});
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
@@ -10,15 +17,14 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if(user) return res.status(400).send('This e-mail is already in use..');
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  })
+
+  user = new User(_.pick(req.body, ['name', 'email', 'password']))
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
 
-  res.send(user);
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
