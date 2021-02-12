ca814885bccd889ac37de63cea26c2cd8d3cdb47
sebslon <sebastesterprogram@gmail.com>
2021-02-12 17:58:31 +0100

    Simplify returns.js route

--- models/rental.js
+++ models/rental.js
@@ -1,71 +1,85 @@
 const mongoose = require("mongoose");
-const Joi = require('joi')
+const moment = require('moment');
+const Joi = require("joi");
 
-const Rental = mongoose.model(
-  "Rental",
-  new mongoose.Schema({
-    customer: {
-      type: new mongoose.Schema({
-        name: {
-          type: String,
-          required: true,
-          minlength: 5,
-          maxlength: 50,
-        },
-        isGold: {
-          type: Boolean,
-          default: false,
-        },
-        phone: {
-          type: String,
-          required: true,
-          minlength: 7,
-          maxlength: 10,
-        },
-      }),
-      required: true,
-    },
-    movie: {
-      type: new mongoose.Schema({
-        title: {
-          type: String,
-          required: true,
-          trim: true,
-          minlength: 5,
-          maxlength: 100,
-        },
-        dailyRentalRate: {
-          type: Number,
-          required: true,
-          min: 0,
-          max: 200,
-        },
-      }),
-      required: true,
-    },
-    dateOut: {
-      type: Date,
-      required: true,
-      default: Date.now,
-    },
-    dateReturned: {
-      type: Date,
-    },
-    rentalFee: {
-      type: Number,
-      min: 0,
-    },
-  })
-);
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+      },
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 7,
+        maxlength: 10,
+      },
+    }),
+    required: true,
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 100,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 200,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  },
+});
+
+rentalSchema.statics.lookup = function (customerId, movieId) {
+  return this.findOne({
+    "customer._id": customerId,
+    "movie._id": movieId,
+  });
+};
+
+rentalSchema.methods.return = function() {
+  this.dateReturned = new Date();
+
+  const rentalDays = moment().diff(this.dateOut, 'days');
+  this.rentalFee = rentalDays * this.movie.dailyRentalRate;
+}
+
+const Rental = mongoose.model("Rental", rentalSchema);
 
 function validateRental(rental) {
   const schema = Joi.object({
     customerId: Joi.objectId().required(),
-    movieId: Joi.objectId().required()
+    movieId: Joi.objectId().required(),
   });
 
   return schema.validate(rental);
-};
+}
 
 exports.Rental = Rental;
 exports.validate = validateRental;
--- routes/returns.js
+++ routes/returns.js
@@ -1,33 +1,34 @@
 const {Rental} = require('../models/rental');
 const {Movie} = require('../models/movie');
 const auth = require('../middleware/auth');
-const moment = require('moment');
+const validate = require('../middleware/validate');
+const Joi = require('joi')
 const express = require('express');
 const router = express.Router();
 
-router.post('/', auth, async (req, res) => {
-  if(!req.body.customerId) return res.status(400).send('CustomerId not provided');
-  if(!req.body.movieId) return res.status(400).send('movieId not provided');
-
-  const rental = await Rental.findOne({
-    'customer._id': req.body.customerId,
-    'movie._id': req.body.movieId,
-  })
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
   if(!rental) return res.status(404).send('Rental not found');
 
   if(rental.dateReturned) return res.status(400).send('Return is already processed');
 
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, 'days');
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
-
+  rental.return();
   await rental.save();
 
   await Movie.update({ _id: rental.movie._id }, {
     $inc: { numberInStock: 1 }
   })
 
-  return res.status(200).send(rental);
+  return res.send(rental);
 });
+
+function validateReturn(req) {
+  const schema = Joi.object({
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  });
+
+  return schema.validate(req);
+}
 
 module.exports = router;
